63b04649b88b1083347a2deaf7a255c8fbfa12c3
jacekkowalczyk <s29829@pjwstk.edu.pl>
2024-04-07 15:16:24 +0200

    v001

---
+++ src/StudentGroup.java
@@ -0,0 +1,12 @@
+public class StudentGroup {
+    String fname = String();
+    String lname = String();
+    int indexNumber;
+
+    String email = String();
+    String address = String();
+double grades
+
+
+
+}
